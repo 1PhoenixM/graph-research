6ac7349a822734a313804d51a6129e51d67d07c4
Melissa <phoenixemere@gmail.com>
2016-04-17 18:19:22 -0400

    Latest graphs

---
+++ 2016SmallGraphSize8Net.sql
@@ -0,0 +1,38 @@
+INSERT INTO Vertices (id) VALUES 
+(0);INSERT INTO Vertices (id) VALUES 
+(1);INSERT INTO Vertices (id) VALUES 
+(2);INSERT INTO Vertices (id) VALUES 
+(3);INSERT INTO Vertices (id) VALUES 
+(4);INSERT INTO Vertices (id) VALUES 
+(5);INSERT INTO Vertices (id) VALUES 
+(6);INSERT INTO Vertices (id) VALUES 
+(7);
+INSERT INTO Edges (vertexFrom, vertexTo) VALUES (0, 3);
+
+INSERT INTO Edges (vertexFrom, vertexTo) VALUES (0, 5);
+
+INSERT INTO Edges (vertexFrom, vertexTo) VALUES (0, 6);
+
+INSERT INTO Edges (vertexFrom, vertexTo) VALUES (0, 7);
+
+INSERT INTO Edges (vertexFrom, vertexTo) VALUES (1, 3);
+
+INSERT INTO Edges (vertexFrom, vertexTo) VALUES (1, 4);
+
+INSERT INTO Edges (vertexFrom, vertexTo) VALUES (1, 6);
+
+INSERT INTO Edges (vertexFrom, vertexTo) VALUES (2, 3);
+
+INSERT INTO Edges (vertexFrom, vertexTo) VALUES (2, 4);
+
+INSERT INTO Edges (vertexFrom, vertexTo) VALUES (3, 4);
+
+INSERT INTO Edges (vertexFrom, vertexTo) VALUES (3, 5);
+
+INSERT INTO Edges (vertexFrom, vertexTo) VALUES (4, 5);
+
+INSERT INTO Edges (vertexFrom, vertexTo) VALUES (5, 6);
+
+INSERT INTO Edges (vertexFrom, vertexTo) VALUES (5, 7);
+
+INSERT INTO Edges (vertexFrom, vertexTo) VALUES (6, 7);
